2ef06034706464ac147b588fed560ed63a02a50c
sajadMod1 <sgadking1234512345@gmail.com>
2026-05-20 19:37:15 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,14 +1,39 @@
 const express = require('express');
+const crypto = require('crypto');
 const app = express();
 
+const SECRET_KEY = process.env.SECRET_KEY;
+const IV = process.env.IV;
+
+function encrypt(text) {
+    const cipher = crypto.createCipheriv(
+        'aes-256-cbc',
+        Buffer.from(SECRET_KEY),
+        Buffer.from(IV)
+    );
+    let encrypted = cipher.update(text, 'utf8', 'base64');
+    encrypted += cipher.final('base64');
+    return encrypted;
+}
+
+// رابط Update.json
 app.get('/config', async (req, res) => {
     try {
-        const url = process.env.TARGET_URL;
-        const response = await fetch(url);
-        const data = await response.json();
-        res.json(data);
+        const response = await fetch(process.env.TARGET_URL);
+        const data = await response.text();
+        res.send(encrypt(data));
     } catch (err) {
-        res.status(500).json({ error: 'Failed to fetch' });
+        res.status(500).json({ error: 'Failed' });
+    }
+});
+
+
+app.get('/download', async (req, res) => {
+    try {
+        const url = process.env.DOWNLOAD_URL;
+        res.send(encrypt(url));
+    } catch (err) {
+        res.status(500).json({ error: 'Failed' });
     }
 });
 
